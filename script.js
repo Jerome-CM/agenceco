3bd89bf581e237e95ce29abd66c24f37aa4ec52c
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-06-25 11:47:49 +0200

    fix display nav error

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
+
+    let screenWith = screen.width;
+
     const menuToggle = document.getElementById("menu-toggle");
     const navMenu = document.getElementById("nav-menu");
+    if(screenWith <= 700){
 
-    menuToggle.addEventListener("click", function() {
-        if(navMenu.style.display === 'flex'){
-            navMenu.style.display='none';
-        } else {
-            navMenu.style.display='flex';
-        }
+        menuToggle.addEventListener("click", function() {
+            if(navMenu.style.display === 'flex'){
+                navMenu.style.display='none';
+            } else {
+                navMenu.style.display='flex';
+            }
 
-    });
-
-    const navLinks = navMenu.querySelectorAll("a");
-
-    navLinks.forEach(link => {
-        link.addEventListener("click", function() {
-            navMenu.style.display='none';
         });
-    });
 
+        const navLinks = navMenu.querySelectorAll("a");
+
+        navLinks.forEach(link => {
+            link.addEventListener("click", function () {
+                navMenu.style.display = 'none';
+            });
+        });
+    }
 });
